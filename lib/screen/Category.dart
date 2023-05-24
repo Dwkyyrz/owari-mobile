9f7a59164b51e3261932461646d849f05d6867f7
Leovander <leovanderadds@gmail.com>
2023-05-24 21:16:45 +0700

    sementara leo iki sek

---
+++ lib/screen/Category.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Category extends StatelessWidget {
+
+  List catList = [
+    "Game Series",
+    "Music Series",
+    "Anime Series"
+  ];
+
+  //sek image list ambek catlist belakangan ae
+
+  List imgList = [
+    "ABCD",
+    "EFGH",
+    "IJKL"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(top: 20, left: 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(padding: EdgeInsets.only(right: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width / 1.5,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF7F8FA),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          label: Text("Find Your Prduct"),
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search,
+                          size: 30,
+                          color: Colors.grey
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Color(1010) 
+                        ),
+                    )
+                  ],
+                ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
