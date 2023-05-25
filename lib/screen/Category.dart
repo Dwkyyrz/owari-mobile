b417d820d11379f90426dd2c9c80176549ce8a2d
Leovander <leovanderadds@gmail.com>
2023-05-25 09:37:47 +0700

    sementara leo iki sek

--- lib/screen/Category.dart
+++ lib/screen/Category.dart
@@ -7,15 +7,7 @@ class Category extends StatelessWidget {
 
   //sek image list ambek catlist belakangan ae
 
-<<<<<<< HEAD
-  List imgList = [
-    "banner1",
-    "",
-    ""
-  ];
-=======
-  List imgList = ["ABCD", "EFGH", "IJKL"];
->>>>>>> 2d82ec620e5753ec7381ee0ff47be9c3ff3f19ba
+  List imgList = ["banner1", "", ""];
 
   @override
   Widget build(BuildContext context) {
@@ -47,39 +39,29 @@ class Category extends StatelessWidget {
                           ),
                         ),
                       ),
-<<<<<<< HEAD
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        color: Color(0xFFF7F8FA),
-                        borderRadius: BorderRadius.circular(10) 
-                        ),
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color: Color(0xFFF7F8FA),
+                            borderRadius: BorderRadius.circular(10)),
                         child: Icon(
                           Icons.notifications_none,
                           size: 30,
                           color: Colors.grey,
                         ),
-                    )
-                  ],
-                ),
-=======
-                      Container(
-                        padding: EdgeInsets.all(15),
-                        decoration: BoxDecoration(color: Color(1010)),
                       )
                     ],
                   ),
->>>>>>> 2d82ec620e5753ec7381ee0ff47be9c3ff3f19ba
                 ),
                 Container(
                   margin: EdgeInsets.only(right: 25, top: 20),
                   alignment: Alignment.center,
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(30),
-                    child: Image.asset("assets/banner1.png",
-                    width: MediaQuery.of(context).size.width / 1.2,
-                    fit: BoxFit.contain,
+                    child: Image.asset(
+                      "assets/banner1.png",
+                      width: MediaQuery.of(context).size.width / 1.2,
+                      fit: BoxFit.contain,
                     ),
                   ),
                 ),
@@ -89,21 +71,21 @@ class Category extends StatelessWidget {
                     padding: EdgeInsets.only(top: 25),
                     child: Row(
                       children: [
-                        for(int i=0; i<catList.length; i++)
-                        Container(
-                          margin: EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            color: catList[i] == "All"
-                            ? Color(0xFFFD725A) : Color.fromARGB(255, 205, 205, 205),
-                            borderRadius: BorderRadius.circular(18)
-                          ),
-                          child: Text(
-                            catList[i],
-                            style: TextStyle(
-                              fontSize: 16,
+                        for (int i = 0; i < catList.length; i++)
+                          Container(
+                            margin: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: catList[i] == "All"
+                                    ? Color(0xFFFD725A)
+                                    : Color.fromARGB(255, 205, 205, 205),
+                                borderRadius: BorderRadius.circular(18)),
+                            child: Text(
+                              catList[i],
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
                             ),
-                          ),
-                        )
+                          )
                       ],
                     ),
                   ),
